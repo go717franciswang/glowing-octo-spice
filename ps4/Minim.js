86a6289a046984bd4a0e742921ce9945d0169c24
Francis <go717franciswang@gmail.com>
2014-03-03 14:25:52 -0800

    Remove logging

--- ps4/Minim.js
+++ ps4/Minim.js
@@ -1,6 +1,5 @@
 function Minim() {
     this.loadSample = function(src) {
-        console.log(src);
         return new AudioSample(src);
     };
 };
@@ -8,7 +7,6 @@ function Minim() {
 function AudioSample(src) {
     this.src = src;
     this.trigger = function() {
-        console.log(this.src);
         (new Audio(this.src)).play();
     };
 };
